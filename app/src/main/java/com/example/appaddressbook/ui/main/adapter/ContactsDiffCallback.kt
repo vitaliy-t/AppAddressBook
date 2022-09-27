87c8812c903cd4df23fbaaf6acb6de146ecc188e
Vit <vitaliytatomyr@gmail.com>
2022-09-27 17:17:44 +0300

    make customerId not nullable

--- app/src/main/java/com/example/appaddressbook/ui/main/adapter/ContactsDiffCallback.kt
+++ app/src/main/java/com/example/appaddressbook/ui/main/adapter/ContactsDiffCallback.kt
@@ -11,9 +11,20 @@ class ContactsDiffCallback(private val oldList: List<Contact>, private val newLi
         return oldList[oldItemPosition].customerId === newList[newItemPosition].customerId
     }
 
-    override fun areContentsTheSame(oldCourse: Int, newPosition: Int): Boolean {
-        val (_, value, name) = oldList[oldCourse]
-        val (_, value1, name1) = newList[newPosition]
-        return name == name1 && value == value1
+    override fun areContentsTheSame(oldPosition: Int, newPosition: Int): Boolean {
+        val contact = oldList[oldPosition]
+        val contact1 = newList[newPosition]
+        return contact.customerId == contact1.customerId &&
+                contact.companyName == contact1.companyName &&
+                contact.contactName == contact1.contactName &&
+                contact.contactTitle == contact1.contactTitle &&
+                contact.address == contact1.address &&
+                contact.city == contact1.city &&
+                contact.email == contact1.email &&
+                contact.region == contact1.region &&
+                contact.postalCode == contact1.postalCode &&
+                contact.country == contact1.country &&
+                contact.phone == contact1.phone &&
+                contact.fax == contact1.fax
     }
 }
