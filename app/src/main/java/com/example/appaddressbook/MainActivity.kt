c2b506c89f57ba3bff7c7d94e028d595bd5f49d9
Vit <vitaliytatomyr@gmail.com>
2022-09-28 13:01:13 +0300

    implement navigation for shortcut

--- app/src/main/java/com/example/appaddressbook/MainActivity.kt
+++ app/src/main/java/com/example/appaddressbook/MainActivity.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.example.appaddressbook.databinding.ActivityMainBinding
+import com.example.appaddressbook.ui.main.MainFragmentDirections
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -26,7 +28,10 @@ class MainActivity : AppCompatActivity() {
     private fun handleIntent(intent: Intent) {
         val addNewContact = intent.extras?.getBoolean(ADD_CONTACT_SHORTCUT, false) ?: false
         if (addNewContact) {
-            // TODO Navigate to adding new contact
+            binding?.root?.post {
+                Navigation.findNavController(this, R.id.nav_host_fragment)
+                    .navigate(MainFragmentDirections.toEditContact(null) )
+            }
         }
     }
 }
