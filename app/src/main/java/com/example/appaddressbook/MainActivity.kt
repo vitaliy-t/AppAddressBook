6b28d83c799f7ddc5eb593ff9b4e5e7f6a2c3f75
Vit <vitaliytatomyr@gmail.com>
2022-09-28 09:25:00 +0300

    Add navigation component

--- app/src/main/java/com/example/appaddressbook/MainActivity.kt
+++ app/src/main/java/com/example/appaddressbook/MainActivity.kt
@@ -3,20 +3,18 @@ package com.example.appaddressbook
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.appaddressbook.ui.main.MainFragment
+import androidx.databinding.DataBindingUtil
+import com.example.appaddressbook.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         intent?.let(::handleIntent)
     }
 
